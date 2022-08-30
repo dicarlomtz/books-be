6b035cbfe2225a56750471a192f198f093a87e44
dicarlomtz <dicarlomtz@gmail.com>
2022-08-30 14:09:38 -0600

    Removing intervention package config from config/app

--- config/app.php
+++ config/app.php
@@ -195,8 +195,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Intervention\Image\ImageServiceProvider::class
-
     ],
 
     /*
@@ -211,7 +209,6 @@ return [
     */
 
     'aliases' => Facade::defaultAliases()->merge([
-        'Image' => Intervention\Image\Facades\Image::class
     ])->toArray(),
 
 ];
