b677172387ff80633e05efdb18ee53eabab15d25
dicarlomtz <dicarlomtz@gmail.com>
2022-09-16 13:31:07 -0600

    Configuring CORS

--- app/Http/Middleware/ForceJsonResponse.php
+++ app/Http/Middleware/ForceJsonResponse.php
@@ -7,14 +7,7 @@ use Illuminate\Http\Request;
 
 class ForceJsonResponse
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
-     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
-     */
-    public function handle(Request $request, Closure $next)
+   public function handle(Request $request, Closure $next)
     {
         $request->headers->set('Accept', 'application/json');
         return $next($request);
--- config/cors.php
+++ config/cors.php
@@ -15,9 +15,9 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['/*', 'sanctum/csrf-cookie'],
 
-    'allowed_methods' => ['*'],
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE'],
 
     'allowed_origins' => ['*'],
 
